a8dea9fdd4bfa6a74fd5520ffe30c918ad5941ee
Joe <J_rdavid24@outlook.com>
2024-11-20 23:10:27 -0600

    updates

---
+++ scripts/validation.js
@@ -0,0 +1,40 @@
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+
+function setEventListeners(formEl, options) {
+  const { inputSelector } = options;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  console.log(inputEls);
+}
+
+function enableValidation(options) {
+  const formEls = [...document.querySelectorAll(options.formSelector)];
+  formEls.forEach((formEl) => {
+    formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+
+    setEventListeners(formEl, options);
+    // look for all inputs of form
+    //  loop through all the inputs to see if are all valid
+    // if input is not valid
+    // get validation message
+    // add error class to input
+    // display error message
+    // disabled button
+    // if all inputs are
+    // enable button
+    // reset error messages
+  });
+}
+
+const config = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
+enableValidation(config);

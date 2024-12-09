d5c7618bef745d29d5bdbac154d278d156f49d28
Joe <J_rdavid24@outlook.com>
2024-12-09 11:58:41 -0600

    update

---
+++ scripts/Card.js
@@ -0,0 +1,15 @@
+export default class Card {
+  constructor({ name, link }, cardSelector) {
+    this._name = name;
+    this._link = link;
+    this._cardSelector = cardSelector;
+  }
+
+  _setEventListeners() {}
+
+  getView() {
+    // get the card view
+    // set event listeners
+    // return the card
+  }
+}

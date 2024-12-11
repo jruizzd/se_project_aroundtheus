bd7b737bd8bffc10e4f15a34e64b90e7bd53717c
Joe <J_rdavid24@outlook.com>
2024-12-10 23:25:27 -0600

    update

--- scripts/Card.js
+++ scripts/Card.js
@@ -5,11 +5,41 @@ export default class Card {
     this._cardSelector = cardSelector;
   }
 
-  _setEventListeners() {}
+  _setEventListeners() {
+    //".card__like-button"
+    const likeButton = this._cardElement
+      .querySelector(".card__like-button")
+      .addEventlistener("click", () => {
+        this._handleLikeIcon();
+      });
+
+    // ".card__delete-button"
+    const deleteButton = this._cardElement
+      .querySelector(".card__delete-button")
+      .addEventlistener("click", () => {
+        this._handleDeleteCard();
+      });
+  }
+
+  _handleDeleteCard() {
+    this._cardElement.remove();
+    this._cardElement = null;
+  }
+
+  _handleLikeIcon() {
+    this._cardElement
+      .querySelector(".card__like-button")
+      .classList.toggle(".card__like-button_active");
+  }
 
   getView() {
+    this._cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
     // get the card view
     // set event listeners
+    this._setEventListeners();
     // return the card
   }
 }

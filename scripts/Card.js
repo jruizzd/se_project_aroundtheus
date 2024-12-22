7cf277625c857132de07956a613ddce8683071d1
Joe <J_rdavid24@outlook.com>
2024-12-22 16:18:06 -0600

    updates

--- scripts/Card.js
+++ scripts/Card.js
@@ -29,7 +29,7 @@ export default class Card {
   _handleLikeIcon() {
     this._cardElement
       .querySelector(".card__like-button")
-      .classList.toggle(".card__like-button_active");
+      .classList.toggle("card__like-button_active");
   }
 
   getView() {

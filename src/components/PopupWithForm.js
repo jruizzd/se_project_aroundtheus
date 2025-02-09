44c529d222df85d8b3b357da28cacb707517569c
Joe <J_rdavid24@outlook.com>
2025-02-08 23:13:44 -0600

    updates

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -3,11 +3,12 @@
 import Popup from "./Popup.js";
 
 // inherits a class
-class PopupWithForm extends Popup {
+export default class PopupWithForm extends Popup {
   constructor(popupSelector, handleFormSubmit) {
     super({ popupSelector });
     this._popupForm = this._popupElement.querySelector(".modal__form");
     this._handleFormSubmit = handleFormSubmit;
+    this._inputData = {};
   }
 
   close() {
@@ -16,5 +17,22 @@ class PopupWithForm extends Popup {
     super.close();
   }
 
-  _getInputValues() {}
+  setEventListeners() {
+    // sets event listeners
+    super.setEventListeners();
+
+    this._popupForm.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+    });
+  }
+
+  // collects data from all the input fields and returns it as an object. This data should then be passed to the submission handler as an argument.
+  _getInputValues() {
+    const inputValues = this._popupForm.querySelectorAll(".modal__input");
+    inputValues.forEach((input) => {
+      this._inputData[input.name] = input.value;
+    });
+    return this._inputData;
+  }
 }

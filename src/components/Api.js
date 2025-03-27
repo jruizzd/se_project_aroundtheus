a46f4702a9715428922b83864f6d83862dea70c8
Joe <J_rdavid24@outlook.com>
2025-03-27 02:17:22 -0500

    updated

---
+++ src/components/Api.js
@@ -0,0 +1,66 @@
+// creating the API class
+export default class Api {
+  constructor(options) {
+    // stores base Url for all API requests in "https://around-api.en.tripleten-services.com/v1/cards"
+    this._baseUrl = options.baseUrl;
+    //  stores headers used in all request like authorization: API key and Content-Type: tells the server we're sending JSON data
+    this._headers = options.headers;
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "GET",
+      headers: this._headers,
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  // create the method which fetches to get the current user's info
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      // method: "GET",
+      headers: this._headers,
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  editProfile({ name, about }) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        about: about,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  addCard({ name, link }) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        link: link,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Error: ${res.status}`);
+    });
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,8 @@ import PopupWithForm from "../components/PopupWithForm.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import Section from "../components/Section.js";
 import UserInfo from "../components/UserInfo.js";
-import { initialCards, formSettings } from "../utils/constants.js";
+import { formSettings } from "../utils/constants.js";
+import Api from "../components/Api.js";
 import "../pages/index.css";
 
 // Card.js and Validator.js
@@ -31,11 +32,43 @@ profileFormValidator.enableValidation();
 // cardInstance.init();
 // Handle profile modal opening and form resetting
 
-const cardSection = new Section(
-  { items: initialCards, renderer: renderCard },
-  ".cards__list"
-);
-cardSection.renderItems();
+const api = new Api({
+  baseUrl: "https://around-api.en.tripleten-services.com/v1",
+  headers: {
+    authorization: "106a4ee2-1a86-4403-8374-54ef205a87e1",
+    "Content-Type": "application/json",
+  },
+});
+
+let cardSection;
+
+// getting the cards on the server
+api
+  .getInitialCards()
+  .then((cards) => {
+    // we want to display the cards from the server on our page
+    cardSection = new Section(
+      {
+        items: cards.reverse(),
+        renderer: renderCard,
+      },
+      ".cards__list"
+    );
+    cardSection.renderItems();
+  })
+  .catch((err) => {
+    console.error(err); // log the error to the console
+  });
+
+// call the getUserInfo method, to get the current user's name, description, etc...
+// and then, display their name and description on the page
+api
+  .getUserInfo()
+  .then((userData) => {
+    userInfo.setUserInfo(userData.name, userData.about);
+  })
+  .catch((err) => console.error(err));
+
 // Template
 const cardTemplate = document
   .querySelector("#card-template")
@@ -58,6 +91,7 @@ const userInfo = new UserInfo({
   nameSelector: ".profile__title",
   jobSelector: ".profile__description",
 });
+
 // const userInfoData = userInfo.getUserInfo();
 // newCardPopup.close();
 // Wrappers
@@ -97,15 +131,32 @@ function renderCard(cardData) {
 function handleProfileFormSubmit(inputValues) {
   // utilize the inputValues object
   // and call the setUserInfo method (in the UserInfo class) to set the name and description that was typed in by the user
-  userInfo.setUserInfo(inputValues.name, inputValues.description);
+  api
+    .editProfile({ name: inputValues.name, about: inputValues.description })
+    .then((res) => {
+      userInfo.setUserInfo(res.name, res.about);
+    })
+    .catch(console.error);
 }
 
 function handleAddCardFormSubmit(inputValues) {
   const name = inputValues.title;
   const link = inputValues.url;
-  renderCard({ name, link });
-  cardFormElement.reset();
-  cardFormValidator.toggleSubmitButton();
+  if (!link.startsWith("http://") && !link.startsWith("https://")) {
+    console.error("Please enter a valid URL starting with http:// or https://");
+    return;
+  }
+  api
+    .addCard({
+      name: name,
+      link: link,
+    })
+    .then((newCard) => {
+      renderCard(newCard);
+      cardFormElement.reset();
+      cardFormValidator.toggleSubmitButton();
+    })
+    .catch((err) => console.error(err));
 }
 
 const popupWithImage = new PopupWithImage("#preview-image-modal");
@@ -126,3 +177,14 @@ profileEditButton.addEventListener("click", () => {
 
 // add new card
 addNewCardButton.addEventListener("click", () => newCardPopup.open());
+
+// making a request to server
+// fetch("https://around-api.en.tripleten-services.com/v1/cards", {
+//   headers: {
+//     authorization: "106a4ee2-1a86-4403-8374-54ef205a87e1",
+//   },
+// })
+//   .then((res) => res.json())
+//   .then((result) => {
+//     console.log(result);
+//   });
